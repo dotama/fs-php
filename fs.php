98d5d60eba417dde3125f0c5b54d5779c7aae10a
Stephan Zeissler <stephan@giantswarm.io>
2016-01-04 21:38:14 +0100

    Use bcrypt to hash passwords

--- fs.php
+++ fs.php
@@ -154,15 +154,20 @@ class KeyManager {
 	public function KeyManager() {
 		$this->keys = array();
 	}
-	public function addKey($name, $password) {
-		$this->keys[] = array(
+	public function addBcryptCredentials($name, $hash) {
+		$key = array(
 			'access' => $name,
-			'secret' => $password
+			'secret' => $hash
 		);
+		$this->keys[] = $key;
 	}
+	public function addKey($name, $password) {
+		$this->addBcryptCredentials($name, password_hash($password, PASSWORD_DEFAULT));
+	}
+
 	public function validCredentials($name, $password) {
 		foreach ($this->keys AS $credentialPair) {
-			if ($credentialPair['access'] == $name && $credentialPair['secret'] == $password) {
+			if ($credentialPair['access'] == $name && password_verify($password, $credentialPair['secret'])) {
 				return true;
 			}
 		}
