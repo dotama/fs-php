d52ea4eaffba6a63e107304d5d43700e6c0f36f4
Stephan Zeissler <stephan@giantswarm.io>
2016-03-25 03:17:55 +0100

    Renamed files

--- lib/autoload.php
+++ lib/autoload.php
@@ -1,6 +1,11 @@
 <?php
 
-require_once(__DIR__ . '/iam.php');
+# general authnz
+require_once(__DIR__ . '/AccessManager.php');
+require_once(__DIR__ . '/KeyManager.php');
+
+# fs-php specific
 require_once(__DIR__ . '/ACL.php');
-require_once(__DIR__ . '/filesystem.php');
+require_once(__DIR__ . '/Server.php');
 require_once(__DIR__ . '/MessagingService.php');
+require_once(__DIR__ . '/LocalBucket.php');
