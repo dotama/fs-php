9dea1d0d8ec44d0f71ec671b5c82b311725998ae
Stephan Zeissler <stephan.zeissler@rewe-digital.com>
2016-11-17 01:08:40 +0100

    Temporarily break a test for testing the notification

--- tests/AccessManagerTest.php
+++ tests/AccessManagerTest.php
@@ -35,7 +35,7 @@ class AccessManagerTest extends PHPUnit_Framework_TestCase {
 
   public function testConditions() {
     $accessManager = $this->givenAccessManagerWithConditionedPolicy();
-    $this->assertTrue($accessManager->isGranted('/data/secret', 'zeisss', 'mfs::ReadObject'));
+    $this->assertfalse($accessManager->isGranted('/data/secret', 'zeisss', 'mfs::ReadObject'));
   }
 
   public function testDenyPolicyHavePrecedense() {
